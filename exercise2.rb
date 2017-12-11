7aed04917ade229efa20011648ad9306f50a1ea3
John Carlo Lopez <condottiere.jc@gmail.com>
2017-12-11 13:25:06 -0500

    complete step 3 and 4

--- exercise2.rb
+++ exercise2.rb
@@ -4,3 +4,10 @@ puts "The total for your meal will be $#{(total*1.15).round(2)} (15% tip:#{total
 #step 2
 puts "Number of meals:" + 2.to_s
 #step 3
+puts "45628 multiplied by 7839 equals #{45628 * 7839}"
+#step 4
+#(10 < 20 && 30 < 20) || !(10 == 11)
+# (true && false) || !(false)
+# false || true
+# true
+puts (10 < 20 && 30 < 20) || !(10 == 11)
